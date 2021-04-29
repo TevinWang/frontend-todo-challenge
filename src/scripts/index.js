60af1859669b0c3c593746abf21d2a319318a84b
TevinWang <tevin.wang@warriorlife.net>
2021-04-29 00:15:59 -0700

    pog

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,7 +7,20 @@ const todoItemsEl = document.getElementById('todo-items');
 const completedItemsEl = document.getElementById('completed-items');
 
 inputEl.addEventListener('keyup', e => {
-  if (e.key === 'Enter') {
+    if (e.key === 'Enter' && inputEl.value != '') {
+        var item = document.createElement("li");
+        var itemValue = document.createTextNode(inputEl.value);
+        item.appendChild(itemValue);
 
-  }
+        todoItemsEl.appendChild(item);
+        todos.push(item);
+
+        item.addEventListener('click', e => {
+            if (todoItemsEl.contains(item)) {
+                completedItemsEl.appendChild(item);
+            } else {
+                todoItemsEl.appendChild(item);
+            }
+        })
+    }
 });
